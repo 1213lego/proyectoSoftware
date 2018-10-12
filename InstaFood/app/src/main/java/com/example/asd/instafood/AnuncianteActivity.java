680cc1d7453164605f43c270a620e627ce0ddc74
Gabriel <Gabriel09199@gmail.com>
2018-10-11 22:43:14 -0500

    Activity
    Perfil del usuario y del anunciante

---
+++ InstaFood/app/src/main/java/com/example/asd/instafood/AnuncianteActivity.java
@@ -0,0 +1,13 @@
+package com.example.asd.instafood;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AnuncianteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_anunciante);
+    }
+}
