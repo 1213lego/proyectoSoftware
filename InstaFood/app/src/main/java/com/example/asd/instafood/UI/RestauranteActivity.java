411f3df9e67f23f650f056328bcaf0441c399c03
sercel98 <sergiocelemin1998@gmail.com>
2018-11-13 10:17:56 -0500

    Creación Restaurante activity

---
+++ InstaFood/app/src/main/java/com/example/asd/instafood/UI/RestauranteActivity.java
@@ -0,0 +1,15 @@
+package com.example.asd.instafood.UI;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.asd.instafood.R;
+
+public class RestauranteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_restaurante);
+    }
+}
