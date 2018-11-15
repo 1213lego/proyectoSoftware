436e4a3bd3a510f2332478f03826286a497ad168
sercel98 <sergiocelemin1998@gmail.com>
2018-11-14 19:27:13 -0500

    Registro usuario finishi

---
+++ InstaFood/app/src/main/java/com/example/asd/instafood/UI/registroPlatoActivity.java
@@ -0,0 +1,15 @@
+package com.example.asd.instafood.UI;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.asd.instafood.R;
+
+public class registroPlatoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro_plato);
+    }
+}
