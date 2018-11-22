ac7a91a05945e686de269748d7432a521a6bcd6c
1213lego <36804989+1213lego@users.noreply.github.com>
2018-11-22 15:16:23 -0500

    registro full plato

--- InstaFood/app/src/main/java/com/example/asd/instafood/UI/registroPlatoActivity.java
+++ InstaFood/app/src/main/java/com/example/asd/instafood/UI/registroPlatoActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AlertDialog;
@@ -23,6 +24,7 @@ import com.example.asd.instafood.ViewModels.RegistroPlatoViewModel;
 import com.example.asd.instafood.db.models.Plato;
 
 import java.io.ByteArrayOutputStream;
+import java.io.File;
 
 
 public class RegistroPlatoActivity extends AppCompatActivity {
@@ -33,22 +35,47 @@ public class RegistroPlatoActivity extends AppCompatActivity {
     private final int PHOTO_CODE = 200;
     private final int SELECT_PICTURE = 300;
 
+    private String mPath;
     private Button btnExaminar;
     private ImageView imgPlato;
     private int idRestaurante;
     private EditText nombrePlato;
     private EditText descripcionPlato;
     private RegistroPlatoViewModel viewModel;
+    private Button registrarPlato;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro_plato);
         viewModel=ViewModelProviders.of(this).get(RegistroPlatoViewModel.class);
         btnExaminar = (Button) findViewById(R.id.btnExaminar);
-        imgPlato = (ImageView) findViewById(R.id.imagenPlato);
+        imgPlato = (ImageView) findViewById(R.id.imagenPlatoRegistro);
         idRestaurante=getIntent().getIntExtra("Id",-1);
         nombrePlato=findViewById(R.id.txtNombrePlato);
         descripcionPlato=findViewById(R.id.txtDescripcionPlato);
+        registrarPlato=findViewById(R.id.btnFinalizar);
+        registrarPlato.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //todo agregar el plato a la db
+                if(TextUtils.isEmpty(descripcionPlato.getText())|| TextUtils.isEmpty(nombrePlato.getText()))
+                {
+                    Toast.makeText(RegistroPlatoActivity.this, "No has llenado los campos", Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    Bitmap bitmap = ((BitmapDrawable) imgPlato.getDrawable()).getBitmap();
+                    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                    bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+                    byte[] imageInByte = baos.toByteArray();
+                    Plato plato= new Plato(idRestaurante,nombrePlato.getText().toString(),descripcionPlato.getText().toString(),imageInByte);
+                    viewModel.ingresar(plato);
+                    Intent intent= new Intent();
+                    setResult(RESULT_OK,intent);
+                    finish();
+                }
+            }
+        });
         btnExaminar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -91,6 +118,7 @@ public class RegistroPlatoActivity extends AppCompatActivity {
             if (requestCode == PHOTO_CODE) {
                 Bitmap bitmap = (Bitmap) data.getExtras().get("data");
                 imgPlato.setImageBitmap(bitmap);
+                galleryAddPic();
 
             } else if (requestCode == SELECT_PICTURE) {
                 Uri imageUri = data.getData();
@@ -99,29 +127,16 @@ public class RegistroPlatoActivity extends AppCompatActivity {
         }
     }
 
-    public void openAnuncianteActivity(View view)
-    {
-        if(view.getId() == R.id.btnFinalizar)
-        {
-            //todo agregar el plato a la db
-            if(TextUtils.isEmpty(descripcionPlato.getText())|| TextUtils.isEmpty(nombrePlato.getText()))
-            {
-                Toast.makeText(this, "No has llenado los campos", Toast.LENGTH_SHORT).show();
-            }
-            else
-            {
-                Bitmap bitmap = ((BitmapDrawable) imgPlato.getDrawable()).getBitmap();
-                ByteArrayOutputStream baos = new ByteArrayOutputStream();
-                bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
-                byte[] imageInByte = baos.toByteArray();
-                Plato plato= new Plato(idRestaurante,nombrePlato.getText().toString(),descripcionPlato.getText().toString(),imageInByte);
-                viewModel.ingresar(plato);
-                Intent intent= new Intent();
-                setResult(RESULT_OK,intent);
-                finish();
-            }
-        }
+    private void galleryAddPic() {
+        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        Long timestamp = System.currentTimeMillis() / 1000;
+        String imageName = timestamp.toString() + ".jpg";
+
+        mPath = Environment.getExternalStorageDirectory() + File.separator + MEDIA_DIRECTORY
+                + File.separator + imageName;
+        File f = new File(mPath);
+        Uri contentUri = Uri.fromFile(f);
+        mediaScanIntent.setData(contentUri);
+        this.sendBroadcast(mediaScanIntent);
     }
-
-
 }
